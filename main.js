c5485767df665e1e8e0979dffa454a9d4beea1c3
Michael Grumbine <mgrumbine3@gmail.com>
2022-01-26 15:22:13 -0800

    v1 of the event listener

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ var authorInput = document.getElementById('name');
 var messageInput = document.getElementById('message');
 
 document.getElementsByTagName('button')[0].addEventListener('click', function (event) {
-  
+  var container = document.createElement('div');
+  container.innerHTML = messageInput.value
+  container.insertAdjacentHTML('beforeend', `<p>Posted By: <strong>${authorInput.value}</strong>`);
+  postsElement.appendChild(container);
 });
